d6e32e8f620f9daab45238045008cbf41a72c4ae
Tehmeena <tehmeenazafar814@gmail.com>
2025-10-15 20:00:28 +0500

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from 'components/layout/Header'
 import Footer from 'components/layout/Footer'
 import Home from 'pages/Home'
+import Shop from 'pages/Shop'
 import Contact from 'pages/Contact'
 import About from 'pages/About'
 import Shipping from 'pages/Shipping'
@@ -16,6 +17,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/shipping" element={<Shipping />} />
