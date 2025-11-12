7b0e24033e6847831aad7bcc9dbd7b2504ac2f0c
Taha <11944803+anilhassan512@users.noreply.github.com>
2025-11-12 07:14:21 -0600

    deployment 8

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -71,7 +71,7 @@ function ProductInfo({
       const cartItem = {
         id: productId,
         name,
-        price: String(price).replace('$', ''),
+        price: price,
         image: image || '', // fallback to empty string if undefined
         ...(colors.length > 0 && selectedColor && { color: selectedColor }),
         ...(sizes.length > 0 && selectedSize && { size: selectedSize }),
